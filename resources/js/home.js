5e3fd753660e4eee409c5aa333186a7956d2262e
Titanstic <mgminkhant301999@gmail.com>
2021-07-05 19:11:43 +0630

    change animation,coffee shop img and add js

---
+++ resources/js/home.js
@@ -0,0 +1,4 @@
+function mOver(obj) {
+    obj.innerHTML = "<h3>the best coffee shop</h3>";
+    obj.setAttribute("class", "animation");
+}
--- resources/js/main.js
+++
@@ -1,5 +0,0 @@
-function mOver(obj) {
-    obj.style.backgroundColor = "transparent";
-    document.getElementById("animation").setAttribute("class", "animation");
-
-}
